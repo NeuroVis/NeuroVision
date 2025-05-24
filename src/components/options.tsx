6bd868e3022d5ace2219773da2bf404881d54594
irina <alexairinamaria@gmail.com>
2025-05-24 21:52:18 +0300

    added hover cards to drop-downs

--- src/components/options.tsx
+++ src/components/options.tsx
@@ -8,11 +8,24 @@ import {
     SelectTrigger,
     SelectValue
 } from "@/components/ui/select";
+import {HoverCard, HoverCardContent, HoverCardTrigger} from "@/components/ui/hover-card";
 
 export default function Option({title, items}:{title:string, items:string[]}): React.ReactElement {
     return (
         <div className="flex flex-col text-black gap-2">
-            <p className={"text-[16px]"}>{title}</p>
+            <HoverCard >
+                <HoverCardTrigger asChild>
+                    <p className={"text-[16px]"}>{title}</p>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-45 bg-indigo-100">
+                    <div className={"text-[14px]"}>
+                        Click to find out
+                        <br/>
+                        more about this option.
+                    </div>
+                </HoverCardContent>
+            </HoverCard>
+
             <Select>
                 <SelectTrigger className="w-[180px]">
                     <SelectValue placeholder="choose..."/>
